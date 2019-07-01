55beb05db98f17f559e8cfb815155d6ca64cdeff
Kailash Kumawat <31000726+kailash558@users.noreply.github.com>
2019-07-01 11:53:25 +0530

    Add files via upload

---
+++ SDK-V4/LUIS-and-QnA-2KB/BotServices.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class BotServices : IBotServices
+    {
+        public BotServices(IConfiguration configuration)
+        {
+            // Read the setting for cognitive services (LUIS, QnA) from the appsettings.json
+            Dispatch = new LuisRecognizer(new LuisApplication(
+                configuration["LuisAppId"],
+                configuration["LuisAPIKey"],
+                $"https://{configuration["LuisAPIHostName"]}.api.cognitive.microsoft.com"),
+                new LuisPredictionOptions { IncludeAllIntents = true, IncludeInstanceData = true },
+                true);
+
+            SampleQnA = new QnAMaker(new QnAMakerEndpoint
+            {
+                KnowledgeBaseId = configuration["QnAKnowledgebaseId"],
+                EndpointKey = configuration["QnAAuthKey"],
+                Host = configuration["QnAEndpointHostName"]
+            });
+
+
+            SampleQnA1 = new QnAMaker(new QnAMakerEndpoint
+            {
+                KnowledgeBaseId = configuration["QnAKnowledgebaseId1"],
+                EndpointKey = configuration["QnAAuthKey1"],
+                Host = configuration["QnAEndpointHostName1"]
+            });
+        }
+
+        public LuisRecognizer Dispatch { get; private set; }
+        public QnAMaker SampleQnA { get; private set; }
+
+        public QnAMaker SampleQnA1 { get; private set; }
+    }
+}
